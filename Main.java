0aec3c2e4d19a22175ffa7bb2f4ec9f514e78068
AoiTuNa <bestspark123@naver.com>
2024-07-15 11:29:51 +0900

    feat: 우린 춤을 출거에요

--- Main.java
+++ Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         NeedMoreCash nmc = new NeedMoreCash();
         NhnPrac nhnPrac = new NhnPrac();
-
+        nmc.inputName("이지금");
         System.out.println(nmc.hiWorld());
         System.out.println(nhnPrac.getTest());
+        System.out.println(nmc.yareyare());
     }
 }
--- NeedMoreCash.java
+++ NeedMoreCash.java
@@ -1,5 +1,20 @@
 public class NeedMoreCash {
+
+    private String name;
     public String hiWorld(){
         return "HI!";
+    }
+    public void inputName(String name){
+        this.name = name;
+    }
+    public String yareyare(){
+        if(name == null){
+            return "아가씨 이름이 뭐에요?";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("오죠 사마 is time to goto bed");
+        stringBuilder.append("또 핸드폰 하는거에요?");
+        stringBuilder.append(name+"아가씨 지금 잠에 들지 않으면");
+        return stringBuilder.toString();
     }
 }
